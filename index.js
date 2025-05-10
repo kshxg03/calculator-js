cadc1a3df398415feb092c35d69aa960a5efb63f
Kshitiz Shrestha <Kshitizshrestha2003@gmail.com>
2025-05-10 16:37:52 +0545

    Improve Code

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 
-
+// input fields
 var num1 = document.getElementById('num1');
 var num2 = document.getElementById('num2');
 var primeNum = document.getElementById('primeNum');
-var sqrNum = document.getElementById('sqrNum');
+var sqrCubNum = document.getElementById('sqrCubNum');
 var rootNum = document.getElementById('rootNum');
 
+// buttons
 var addBtn = document.getElementById('addBtn');
 var subBtn = document.getElementById('subBtn');
 var multiplyBtn = document.getElementById('multiplyBtn');
@@ -17,20 +18,15 @@ var cubBtn = document.getElementById('cubBtn');
 var sqRootBtn = document.getElementById('sqRootBtn');
 var cbRootBtn = document.getElementById('cbRootBtn');
 
+// output 
 var rslt = document.getElementById('result');
-// var primeRslt = document.getElementById('primeOrNot');
-// var sqrRslt = document.getElementById('sqrRslt');
 var outputText = rslt.innerHTML;
 
-
-// num1.addEventListener('keydown', function(event) {
-//     console.log('Key pressed: ' + event.key);
-// });
-
-addBtn.addEventListener('click', function() {
-    number1 = num1.value;
-    number2 = num2.value;
-    sum  = Number(number1) + Number(number2);
+// event listeners
+addBtn.addEventListener('click', function () {
+    let number1 = num1.value;
+    let number2 = num2.value;
+    let sum = Number(number1) + Number(number2);
     rslt.innerHTML = outputText + " " + sum;
     num1.value = "";
     num2.value = "";
@@ -38,46 +34,46 @@ addBtn.addEventListener('click', function() {
 })
 
 
-subBtn.addEventListener('click', function() {
-    number1 = num1.value;
-    number2 = num2.value;
-    diff  = Number(number1) - Number(number2);
+subBtn.addEventListener('click', function () {
+    let number1 = num1.value;
+    let number2 = num2.value;
+    let diff = Number(number1) - Number(number2);
     rslt.innerHTML = outputText + " " + diff;
     num1.value = "";
     num2.value = "";
     // console.log(diff);
 })
 
-multiplyBtn.addEventListener('click', function() {
-    number1 = num1.value;
-    number2 = num2.value;
-    product  = Number(number1) * Number(number2);
+multiplyBtn.addEventListener('click', function () {
+    let number1 = num1.value;
+    let number2 = num2.value;
+    let product = Number(number1) * Number(number2);
     rslt.innerHTML = outputText + " " + product;
     num1.value = "";
     num2.value = "";
     // console.log(diff);
 })
 
-divideBtn.addEventListener('click', function() {
-    number1 = num1.value;
-    number2 = num2.value;
-    division  = Number(number1) / Number(number2);
+divideBtn.addEventListener('click', function () {
+    let number1 = num1.value;
+    let number2 = num2.value;
+    let division = Number(number1) / Number(number2);
     rslt.innerHTML = outputText + " " + division;
     num1.value = "";
     num2.value = "";
     // console.log(diff);
 })
 
-primeCheckBtn.addEventListener('click', function() {
-    n = primeNum.value;
+primeCheckBtn.addEventListener('click', function () {
+    let n = primeNum.value;
     if (n <= 1) {
         rslt.innerHTML = n + " is not a prime number.";
     } else {
-        let isPrime = true;  // Assume n is prime unless proven otherwise
-        for (var i = 2; i <= Math.sqrt(n); i++) {  // No need to check beyond sqrt(n)
+        let isPrime = true;  
+        for (var i = 2; i <= Math.sqrt(n); i++) { 
             if (n % i == 0) {
                 isPrime = false;
-                break;  // Exit loop as we found a divisor
+                break; 
             }
         }
         if (isPrime) {
@@ -89,34 +85,34 @@ primeCheckBtn.addEventListener('click', function() {
     primeNum.value = "";
 });
 
-sqrBtn.addEventListener('click', function() {
-    let num = sqrNum.value;
-    sqrResult = Math.pow(num, 2);
+sqrBtn.addEventListener('click', function () {
+    let num = sqrCubNum.value;
+    let sqrResult = Math.pow(num, 2);
     rslt.innerHTML = sqrResult;
-    sqrNum.value = "";
+    sqrCubNum.value = "";
 })
 
-cubBtn.addEventListener('click', function() {
-    let num = sqrNum.value;
-    sqrResult = Math.pow(num, 3);
-    rslt.innerHTML = sqrResult;
-    sqrNum.value = "";
+cubBtn.addEventListener('click', function () {
+    let num = sqrCubNum.value;
+    let cubResult = Math.pow(num, 3);
+    rslt.innerHTML = cubResult;
+    sqrCubNum.value = "";
 })
 
-sqRootBtn.addEventListener('click', function() {
+sqRootBtn.addEventListener('click', function () {
     let num = rootNum.value;
     let rootResult = Math.sqrt(num);
     rslt.innerHTML = rootResult;
     rootNum.value = "";
 });
 
-cbRootBtn.addEventListener('click', function() {
+cbRootBtn.addEventListener('click', function () {
     let num = rootNum.value;
     let rootResult = Math.cbrt(num);
     rslt.innerHTML = rootResult;
     rootNum.value = "";
 });
 
-clearBtn.addEventListener('click', function() {
+clearBtn.addEventListener('click', function () {
     rslt.innerHTML = outputText;
 })
